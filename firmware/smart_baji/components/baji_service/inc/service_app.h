75c5a7bd1eaf83dcb4a8195bc5e1fe9e2dbd0b46
kiritro <554359126@qq.com>
2025-04-22 18:30:20 +0800

    1.add app basic common task

--- firmware/smart_baji/components/baji_service/inc/service_app.h
+++ firmware/smart_baji/components/baji_service/inc/service_app.h
@@ -22,7 +22,8 @@ extern "C" {
 /*********************************************************************
 * TYPEDEFS
 */
-typedef enum {
+typedef enum
+{
     APP_SYS_MONITOR = 0,
     APP_USER_PIXEL,
     APP_USER_BAJI,
@@ -34,41 +35,47 @@ typedef enum {
     APP_ID_MAX,
 } app_id_t;
 
-typedef enum {
+typedef enum
+{
     APP_TYPE_SYSTEM,  // 系统app类型
     APP_TYPE_USER,    // 用户app类型
 } app_type_t;
 
-typedef struct {
-    bool allow_background;                   // 是否允许后台运行
-    void (*background_task_callback)(void);  // 后台任务回调函数
-    uint32_t execution_interval;             // 后台任务定时执行时间，单位：毫秒
+typedef struct
+{
+    bool allow_background;        // 是否允许后台运行
+    void (*func)(void);           // 后台任务回调函数(后台服务统一管理)
+    uint32_t execution_interval;  // 后台任务定时执行时间，单位：毫秒 最小100ms
     uint32_t interval_calc;
 } app_background_task_t;
 
-typedef enum {
+typedef enum
+{
     APP_FOREGROUND_TASK_TIMED,  // 定时任务
     APP_FOREGROUND_TASK_CUSTOM, // 自定义任务
 } app_foreground_task_type_t;
 
-typedef struct {
+typedef struct
+{
     bool is_running;                    // app是否正在前台运行
     app_foreground_task_type_t type;    // 任务类型
-    void (*timed_task_callback)(void);  // 定时任务回调函数
-    uint32_t execution_interval;        // 定时任务执行时间，单位：毫秒
+    void (*func)(void);                 // 定时任务回调函数
+    uint32_t execution_interval;        // 定时任务执行时间，单位：毫秒 最小100ms
     uint32_t interval_calc;
 } app_foreground_task_t;
 
-typedef struct {
+typedef struct
+{
     bool allow_indev_group;                     // 是否允许输入设备分组
     lv_obj_t *main_control;                     // 主屏幕对象指针
     void (*mian_control_init)(void);            // 主屏幕控件初始化
-    void (*mian_control_event)(lv_event_t *e);  // 事件处理函数指针 -> 单击进入app页面(内部响应事件码)
+    void (*mian_control_event)(lv_event_t *e);  // 主屏幕事件处理函数指针 -> 单击进入app页面(内部响应事件码)
     // lv_event_code_t enter_event_coder;          // 进入app页面的事件码
     // lv_event_code_t setting_event_coder;        // 设置app页面的事件码
 } app_ui_main_control_t;
 
-typedef struct {
+typedef struct
+{
     bool is_registered;  // app是否已注册，true:已注册，false:未注册
     app_id_t app_id;
     app_type_t app_type;
--- firmware/smart_baji/components/baji_service/src/service_app.c
+++ firmware/smart_baji/components/baji_service/src/service_app.c
@@ -35,6 +35,10 @@
  */
 #include <string.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+
 #include "sys_log.h"
 #include "service_app.h"
 
@@ -43,10 +47,22 @@
  */
 #define APP_TAG                        "SERVICE_APP"
 
+#define APP_TASK_STACK_SIZE            (4096)
+#define APP_TASK_PRIORITY              (2)
+#define APP_TASK_NAME                  "APP_TASK"
+
+#define APP_MSG_QUEUE_LENGTH           (20)
+#define APP_MSG_QUEUE_ITEM_SIZE        sizeof(app_msg_t)
+#define APP_FUNCS_TIMER_TICKS          pdMS_TO_TICKS(100) //100mS
+
 /*********************************************************************
 * TYPEDEFS
 */
-
+typedef struct
+{
+    app_id_t app_id;
+    void (*func)(void);
+} app_msg_t;
 
 /*********************************************************************
  * CONSTANTS
@@ -58,6 +74,10 @@
  */
 static app_config_t app_list[APP_ID_MAX];
 
+static TaskHandle_t m_app_task_hdl = NULL;
+static QueueHandle_t m_app_msg_hdl = NULL;
+static TimerHandle_t app_timer;
+
 /*********************************************************************
  * GLOBAL VARIABLES
  */
@@ -66,7 +86,10 @@ static app_config_t app_list[APP_ID_MAX];
 /*********************************************************************
  * LOCAL FUNCTIONS
  */
-
+ static void app_task_handle(void *pvParameters);
+ static void app_timer_callback(TimerHandle_t xTimer);
+ static void app_task_init(void);
+ static void app_msg_send(void *msg, bool in_isr);
 
 /*********************************************************************
  * GLOBAL FUNCTIONS
@@ -76,7 +99,7 @@ static app_config_t app_list[APP_ID_MAX];
 
 void service_app_init(void)
 {
-
+    app_task_init();
 }
 
 bool service_app_register(app_config_t *app_config)
@@ -105,6 +128,96 @@ bool service_app_unregister(app_id_t app_id)
     {
         sys_loge(APP_TAG, "app_id is out of range");
         return false;
+    }
+}
+
+
+static void app_task_init(void)
+{
+    if(m_app_task_hdl == NULL)
+    {
+        if ( pdPASS != xTaskCreate( app_task_handle, APP_TASK_NAME, APP_TASK_STACK_SIZE, NULL, APP_TASK_PRIORITY, NULL ))
+        {
+            sys_loge(APP_TAG, "app task create error!");
+        }
+    }
+}
+
+static void app_task_handle(void *pvParameters)
+{
+    m_app_msg_hdl = xQueueCreate( APP_MSG_QUEUE_LENGTH, APP_MSG_QUEUE_ITEM_SIZE );
+    SYS_ERROR_CHECK(m_app_msg_hdl == NULL);
+
+    app_timer = xTimerCreate("app_timer", APP_FUNCS_TIMER_TICKS, pdTRUE, NULL, app_timer_callback);
+    xTimerStart(app_timer, 0);
+  
+    for(;;)
+    {
+        app_msg_t msg;
+        SYS_ERROR_CHECK( xQueueReceive( m_app_msg_hdl, (void *const)&msg, portMAX_DELAY ) != pdPASS );
+
+        if(msg.app_id < APP_ID_MAX && msg.func != NULL)
+        {
+            msg.func();
+            sys_logd(APP_TAG, "app_id: %d", msg.app_id);
+        }
+    }
+}
+
+static void app_timer_callback(TimerHandle_t xTimer)
+{
+    for (int i = 0; i < APP_ID_MAX; i++)
+    {
+        if (app_list[i].is_registered)
+        {
+            // 后台任务统一调度，降低系统开销
+            if(app_list[i].background_task.allow_background && app_list[i].background_task.func!= NULL)
+            {
+                if(app_list[i].background_task.interval_calc >= app_list[i].background_task.execution_interval)
+                {
+                    app_msg_t msg = {.app_id = i, .func = app_list[i].background_task.func};
+                    app_msg_send(&msg, 0);
+                    app_list[i].background_task.interval_calc = 0;
+                }
+                else
+                {
+                    app_list[i].background_task.interval_calc += APP_FUNCS_TIMER_TICKS;
+                }   
+            }
+            
+            // 轻量前台任务使用统一调度，复杂任务建议独立task处理
+            if(app_list[i].foreground_task.is_running && app_list[i].foreground_task.type == APP_FOREGROUND_TASK_TIMED && app_list[i].foreground_task.func!= NULL)
+            {
+                if(app_list[i].foreground_task.interval_calc >= app_list[i].foreground_task.execution_interval)
+                {
+                    app_msg_t msg = {.app_id = i,.func = app_list[i].foreground_task.func};
+                    app_msg_send(&msg, 0);
+                    app_list[i].foreground_task.interval_calc = 0;
+                }
+                else
+                {
+                    app_list[i].foreground_task.interval_calc += APP_FUNCS_TIMER_TICKS;
+                }
+            }
+        }
+    }
+}
+
+static void app_msg_send(void *msg, bool in_isr)
+{
+    if(m_app_msg_hdl != NULL)
+    {
+        if(in_isr == 0)
+        {
+            SYS_ERROR_CHECK((xQueueSend(m_app_msg_hdl, msg, portMAX_DELAY) != pdPASS));
+        }
+        else
+        {
+            BaseType_t xHigherPriorityTaskWoken;
+            xHigherPriorityTaskWoken = pdFALSE;
+            xQueueSendFromISR( m_app_msg_hdl, msg, &xHigherPriorityTaskWoken );
+            portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+        }
     }
 }
 
